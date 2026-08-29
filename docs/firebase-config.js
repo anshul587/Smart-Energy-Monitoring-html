8c5d025cfb476db222a6cbb8924acfd22d5274d7
anshul587 <anshulninawe809@gmail.com>
2026-08-29 21:50:20 +0530

    Add GitHub Pages dashboard

---
+++ docs/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCQJgHdIb5CkGRhAPOI-ynVfHdNFSo6bs",
+  authDomain: "smart-energy-monitoring-5a2a4.firebaseapp.com",
+  databaseURL: "https://smart-energy-monitoring-5a2a4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smart-energy-monitoring-5a2a4",
+  storageBucket: "smart-energy-monitoring-5a2a4.firebasestorage.app",
+  messagingSenderId: "275361980378",
+  appId: "1:275361980378:web:c9f9ae5742be617e0aaae3"
+};
